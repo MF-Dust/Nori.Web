3a26d444f4d8844dcb0a91e402db048355122280
MF. <128943330+MF-Dust@users.noreply.github.com>
2026-08-29 15:08:49 +0800

    feat: expose reconstructed virtual app models

--- frontend-src/runtime/frontend-runtime.ts
+++ frontend-src/runtime/frontend-runtime.ts
@@ -3,6 +3,11 @@ import { LocalAuthController } from "./auth";
 import { EventRpcClient } from "./event-rpc";
 import { ArcadeMediaClient } from "./media-client";
 import { WorldStore } from "./world-store";
+import { BrowserAppModel } from "../apps/browser";
+import { FilesAppModel } from "../apps/files";
+import { MailAppModel } from "../apps/mail";
+import { MessengerAppModel } from "../apps/messenger";
+import { TerminalAppModel } from "../apps/terminal";
 import { ArtifactService } from "../services/artifacts";
 import { ChatService } from "../services/chat";
 import { DesktopService } from "../services/desktop";
@@ -20,6 +25,11 @@ export class NoriFrontendRuntime {
   readonly desktop: DesktopService;
   readonly chat: ChatService;
   readonly games: GameService;
+  readonly browser: BrowserAppModel;
+  readonly files: FilesAppModel;
+  readonly mail: MailAppModel;
+  readonly messenger: MessengerAppModel;
+  readonly terminal: TerminalAppModel;
 
   private readonly unsubscribeWorld: () => void;
 
@@ -31,6 +41,11 @@ export class NoriFrontendRuntime {
     this.desktop = new DesktopService(this.rpc);
     this.chat = new ChatService(this.arcade, this.world);
     this.games = new GameService(this.arcade, this.world);
+    this.browser = new BrowserAppModel(this.artifacts, this.manifold);
+    this.files = new FilesAppModel(this.artifacts, this.manifold);
+    this.mail = new MailAppModel(this.artifacts, this.manifold);
+    this.messenger = new MessengerAppModel(this.artifacts, this.manifold);
+    this.terminal = new TerminalAppModel(this.manifold);
     this.unsubscribeWorld = this.arcade.onMessage((message) => this.world.consume(message));
   }
 
