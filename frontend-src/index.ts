6e0a4ef07b33c2bf3a3022e8474ecf943cbd97bc
MF. <128943330+MF-Dust@users.noreply.github.com>
2026-09-06 13:16:44 +0800

    feat(frontend): export idle faction production effects

--- frontend-src/index.ts
+++ frontend-src/index.ts
@@ -50,6 +50,7 @@ export {
 export * from "./apps/idle-assistants";
 export * from "./apps/idle-default-data";
 export * from "./apps/idle-economy";
+export * from "./apps/idle-faction-effects";
 export * from "./apps/idle-faction-progression";
 export * from "./apps/idle-generic-upgrades";
 export * from "./apps/idle-manifold";
