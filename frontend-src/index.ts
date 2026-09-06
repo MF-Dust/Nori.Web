bb82440fd39a456e67d3d62fe941026a259f8c37
MF. <128943330+MF-Dust@users.noreply.github.com>
2026-09-06 12:30:51 +0800

    chore(frontend): export idle manifold helpers

--- frontend-src/index.ts
+++ frontend-src/index.ts
@@ -51,6 +51,7 @@ export * from "./apps/idle-default-data";
 export * from "./apps/idle-economy";
 export * from "./apps/idle-faction-progression";
 export * from "./apps/idle-generic-upgrades";
+export * from "./apps/idle-manifold";
 export * from "./apps/idle-presentation";
 export * from "./apps/idle-upgrades";
 export * from "./apps/mail";
